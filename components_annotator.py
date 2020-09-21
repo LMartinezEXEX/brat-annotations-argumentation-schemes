be166b9cf9fec2668b997d00cdf2cefca705ebae
damifur <damian.a.furman@gmail.com>
2020-09-21 17:48:51 -0300

    Cosas del script para pasar a formato bert por un lado y por el otro saque el VerifiableFact del annotation.conf y agregué la conclusión en base a lo que charlamos en nuestra última reunion

--- components_annotator.py
+++ components_annotator.py
@@ -8,8 +8,15 @@ for f in glob.glob("/home/damifur/Escritorio/brat-v1.3_Crunchy_Frog/data1/HateEv
         if ann[1].lstrip().startswith("Collective"):
             tweet_final = []
             label_final = []
-            for parts in tweet.read().split(ann[2]):
-                print(parts)
-            print(ann[2])
-#        print(ann[1])
+            partes = tweet.read().split(ann[2].lstrip())
+            for idx, part in enumerate(partes):
+                for word in part.strip().split(" "):
+                    tweet_final.append(word)
+                    label_final.append("0")
+                if idx != len(partes) - 1:
+                    for wd in ann[2].lstrip().split(" "):
+                        tweet_final.append(wd)
+                        label_final.append("1")
+            print(len(tweet_final))
+            print(len(label_final))
         print("--------------------------------------------------------------------------------------------")
